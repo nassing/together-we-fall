88d966f76e52ab5b91d0a4623210964613344b3b
Norman Assing <norman.assing@telecomnancy.net>
2024-02-12 04:40:46 +0100

    Project init

---
+++ back/src/main/java/nassing/togetherwefall/TogetherWeFallApplication.java
@@ -0,0 +1,13 @@
+package nassing.togetherwefall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherWeFallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogetherWeFallApplication.class, args);
+	}
+
+}
